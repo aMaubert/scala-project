8008ecb7f9a99f1818e5c7c656fe947ca4bb6e88
BRAZE Thomas <tbraze@hotmail.fr>
2020-12-20 17:20:20 +0100

    add change direction func

--- src/main/scala/projetal2020/MowerExecutor.scala
+++ src/main/scala/projetal2020/MowerExecutor.scala
@@ -1,8 +1,38 @@
 package projetal2020
 
+import projetal2020.Direction
+
 object MowerExecutor {
 
-  def executeInstruction(mower: Mower, instruction: Instructions.Value): Mower = ???
+  // TODO : throw exception on default
+  def changeDirection(mower: Mower, rotationInstruction: Instructions.Value): Mower = {
+    rotationInstruction match {
+      case 'R' =>
+        mower.direction match {
+          case Direction.North => mower.direction = Direction.East
+          case Direction.East => mower.direction = Direction.South
+          case Direction.South => mower.direction = Direction.West
+          case Direction.West => mower.direction = Direction.North
+        }
+      case 'L' =>
+        mower.direction match {
+          case Direction.North => mower.direction = Direction.West
+          case Direction.West => mower.direction = Direction.South
+          case Direction.South => mower.direction = Direction.East
+          case Direction.East => mower.direction = Direction.North
+        }
+      case _ => "Invalid Rotation"
+    }
+    mower
+  }
+
+  def executeInstruction(mower: Mower, instruction: Instructions.Value): Mower = {
+    instruction match {
+      case 'L' => print('L')
+      case 'R' => print('R')
+      case 'A' => print('A')
+    }
+  }
 
   def executeInstructionList(mower: Mower, instructions: List[Instructions.Value]): MowerState = ???
 
